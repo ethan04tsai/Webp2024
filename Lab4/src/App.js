3c3c5b5772ba1b11da83a350ba0e727dff153425
ethan04tsai <120005618+ethan04tsai@users.noreply.github.com>
2024-05-01 16:37:12 +0800

    Add files via upload

---
+++ Lab4/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import cgu_login from './cgu_login';
+
+function App() {
+    return (
+    <div className="App">
+        <div>
+            {cgu_login()}
+        </div>
+    </div>
+    );
+}
+
+export default App;
